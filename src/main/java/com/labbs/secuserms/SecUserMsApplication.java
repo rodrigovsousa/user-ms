65c3b4d07a06079cf60da0352c2539b539f11472
ext.rvsousa <ext.rvsousa@unicid.edu.br>
2024-10-17 16:42:52 -0300

    Initial build

---
+++ src/main/java/com/labbs/secuserms/SecUserMsApplication.java
@@ -0,0 +1,13 @@
+package com.labbs.secuserms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecUserMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecUserMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/labbs/secuserms/SecUserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labbs.secuserms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecUserMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
