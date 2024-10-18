1b7129601acdf635c75bc1c562fe92c41ee496d7
ext.rvsousa <ext.rvsousa@unicid.edu.br>
2024-10-18 14:47:47 -0300

    Add database config and initial structure

--- src/main/java/com/labbs/secuserms/SecUserMsApplication.java
+++ src/main/java/com/labbs/secuserms/SecUserMsApplication.java
@@ -2,8 +2,9 @@ package com.labbs.secuserms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SecUserMsApplication {
 
     public static void main(String[] args) {
