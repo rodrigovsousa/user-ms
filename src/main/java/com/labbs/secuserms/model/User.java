26a112189de22826fe535aeb6e9478428d2c4945
ext.rvsousa <ext.rvsousa@unicid.edu.br>
2024-10-18 14:52:58 -0300

    Add User Entity

--- src/main/java/com/labbs/secuserms/model/User.java
+++ src/main/java/com/labbs/secuserms/model/User.java
@@ -1,4 +1,32 @@
 package com.labbs.secuserms.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_USERS")
+
 public class User {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
 }
